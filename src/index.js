4d979fef577d09c5b003e56e4dbcb3277644900a
sebastien <sebastien.boucontet@gmail.com>
2016-09-23 15:41:24 +0200

    Laisser l'exception remonter en cas d'erreur de lecture des fichiers

--- src/index.js
+++ src/index.js
@@ -6,25 +6,14 @@ var fs = require('fs');
 var certSeparator = '-----END CERTIFICATE-----';
 
 module.exports = {
-    parsePathSync: function(pathArray) {
-        var message, success = true;
-        var caList = (pathArray || []).reduce(function (carry, value) {
-            try {
-                return carry.concat(fs.readFileSync(value, {
-                    encoding: 'utf-8'
-                }).split(certSeparator).slice(0, -1).map(function (cert) {
-                    return cert + certSeparator;
-                }));
-            }
-            catch (e) {
-                success = false;
-                return carry;
-            }
+    parsePathSync: function (pathArray) {
+        return (pathArray || []).reduce(function (carry, value) {
+            return carry.concat(fs.readFileSync(value, {
+                encoding: 'utf-8'
+            }).split(certSeparator).slice(0, -1).map(function (cert) {
+                return cert + certSeparator;
+            }));
         }, []);
-        if(success) {
-            return caList;
-        }
-        return false;
     }
 };
 
