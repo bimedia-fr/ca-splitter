eb342a7378fd06478521ccc70517fcc2d6060689
sebastien <sebastien.boucontet@gmail.com>
2016-09-23 15:23:57 +0200

    Import initial MAJ fichier README.md Ajout fichier package.json

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # ca-splitter
 Split CA chain to array of CA
+
+### Installation
+
+```javascript
+npm install ca-splitter --save
+```
+
+### Usage
+
+```javascript
+var caSplitter = require('ca-splitter');
+
+var caList = caSplitter.parsePathSync([
+    'path1/certs_chain.pem',
+    'path2/certificate.pem'
+]);
+```
+
+
---
+++ src/index.js
@@ -0,0 +1,31 @@
+/*jslint node : true, nomen: true, plusplus: true, vars: true, eqeq:true*/
+"use strict";
+
+var certSeparator = '-----END CERTIFICATE-----';
+
+module.exports = {
+    parsePathSync: function(pathArray) {
+        var message, success = true;
+        var caList = (pathArray || []).reduce(function (carry, value) {
+            try {
+                return carry.concat(fs.readFileSync(value, {
+                    encoding: 'utf-8'
+                }).split(certSeparator).slice(0, -1).map(function (cert) {
+                    return cert + certSeparator;
+                }));
+            }
+            catch (e) {
+                success = false;
+                return carry;
+            }
+        }, []);
+        if(success) {
+            return caList;
+        }
+        return false;
+    }
+};
+
+
+
+    
